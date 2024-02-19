8c3a05b22efe8c7c87fe845ba5ce9530fae7758e
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-02-19 09:59:22 +0100

    homework19.02.2024

--- age&name.js
+++ age&name.js
@@ -1,1 +1,7 @@
-console.log("Hellow Word");
+let age = 15;
+let name = "Антон";
+console.log(age);
+console.log(name);
+name = "Антон Манжос";
+console.log(name);
+alert("мене звати " + name + ", мені " + age + "років.");
