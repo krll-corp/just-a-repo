5e283c28d618c4c1e09532c10e7b48537534798e
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-02-22 18:07:08 +0100

    bag

--- hw/rem&pow.js
+++ hw/rem&pow.js
@@ -21,7 +21,6 @@ console.log(a);
 console.log(b);
 console.log(c);
 */
-/*
-let age = +prompt("скільки вам років?");
-console.log(age >= 18
- 
+/* let age = +prompt("скільки вам років?");
+console.log(age >= 18);
+*/
