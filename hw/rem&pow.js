9d5183f6ebeaa9ec10f4cea9af4efa7f67fca538
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-02-22 17:33:12 +0100

    дз

---
+++ hw/rem&pow.js
@@ -0,0 +1,6 @@
+let num1 = +prompt("Перше число");
+let num2 = +prompt("Друге число");
+let rem = num1 % num2;
+let pow = num1 ** num2;
+console.log(`Залишок від ділення  ${num1}, на ${num2} дорівнює (дані зі змінної ${rem})."
+"Результат зведеня ${num1}, у ступінь ${num2} дорівнює ${pow}.`);
