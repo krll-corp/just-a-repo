f668ddc9a37bc636b4b2a016a18ae59f41db9cd8
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-02-26 07:49:06 +0100

    hw

--- hw/ifelse.js
+++ hw/ifelse.js
@@ -3,10 +3,18 @@
 if (num >= 10)
  console.log("число більше або дорівнює 10");
 */
-let pwd = prompt("Який пароль?");
+/*let pwd = prompt("Який пароль?");
 if (pwd == "alala") {
 console.log("Ласкаво просимо!");
 } else{
     console.log("Пароль не вірний");
 }
+*/
 
+let a = prompt("Що небуть");
+if (a == "" ) {
+    console.log("Ви нічого не ввели");
+}
+else if (a === false) {
+    console.log("Відміна вводу")
+}
