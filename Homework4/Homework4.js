bf9fd66d88660893891ccf63039cf5f217734e98
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-03-03 18:35:28 +0100

    Update Homework4.js

--- Homework4/Homework4.js
+++ Homework4/Homework4.js
@@ -1,1 +1,129 @@
+"use strict"
+/*
+let a = +prompt('Введіть число');
+let b = +prompt('Введіть число');
+let message = a > b ? 'Перше число більше' : a === b ? 'Числа рівні' : 'Дргуе число більше';
+alert(message);
+*/
+/*
+let a = prompt('Введіть що небуть');
+switch(a){
+    case null: 
+    console.log('Відміна вводу');
+   break;
+    case "":
+        console.log('Ви нічого не ввели');
+        break;
+        default:
+            console.log(a);
+            break;
+        }
 
+*/
+/*
+let a = +prompt("Введіть номер місяця");
+switch(a){
+    case 1 :
+    case 2: 
+    case 12:
+        console.log("Зима");
+        break;
+        case 3:
+        case 4: 
+        case 5:
+            console.log('Весна');
+            break;
+            case 6: 
+            case 7: 
+            case 8:
+                console.log("Літо");
+                break;
+               case 9: 
+                case 10: 
+                case 11:
+                    console.log("Осінь");
+                    break;
+                    default:
+                        console.log("Невірний номер місяця");
+                        break;
+}
+*/
+/*
+for(let i = 0; i<=10; i++){
+    console.log(i);
+}
+*/
+/*
+let i = 0;
+while(i <= 10){
+    console.log(i);
+    i++;
+}
+*/
+/*
+let a = +prompt("Початок");
+let end = +prompt("Кінець");
+for(let i = a ;i <= end; i++) {
+    if (i % 5 === 0){
+        console.log(i);
+    }
+}
+*/
+/*
+let a = +prompt("Початок");
+let end = +prompt("Кінець");
+while(a < end){
+    if (a % 5 === 0){
+        console.log(a);
+    }
+    a++
+}
+*/
+/*
+let mul;
+for(let i = 0; i < 10; i++){
+    mul = 3*i;
+    console.log(`3 помножити на ${i} дорівнює ${mul}`);
+}
+*/
+/*
+let mul;
+let i = 0;
+
+while (i < 10) {
+    mul = 3 * i;
+    console.log(`3 помножити на ${i} дорівнює ${mul}`);
+    i++;
+}
+*/
+/*
+let i = 0;
+let sum = 0;
+let num = 0;
+while(i < 5){
+    num = +prompt("Введіть число");
+    sum += num;
+    i++;
+}
+*/
+/*
+let a = +prompt("Введіть число");
+let factorial = 1;
+for(let i = 1; i <= a; i++){
+    factorial *= i;
+}
+console.log(factorial);
+*/
+let a = +prompt("Введіть кількість рядків");
+let i = 1;
+let b = "*";
+if (a > 0 && a % 1 === 0){
+while(i < a){
+    console.log(b);
+    b = b + "*";
+    i++;
+}
+}
+else {
+    console.log("Яне знаю таких натуральних чисел");
+}
