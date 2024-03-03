0a6deba46ba00064f84779d977dab1109cb7a615
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-03-03 19:11:03 +0100

    homework functions

---
+++ Homework5/homework5.js
@@ -0,0 +1,32 @@
+"use strict"
+/*function sum(num1, num2){
+    return num1 + num2;
+}
+*/
+/*
+function mulBy5(num){
+    console.log(`5 * ${num} = ${num * 5}`);
+}
+*/
+/*
+function max(num1, num2){
+    if (num1 > num2){
+        return num1;
+    }
+    else if (num1 = num2){
+        return "Числа рівні";
+    }
+    else {
+        return num2;
+    }
+}
+*/
+function pow(x, n){
+    for(i; i<=n; i++){
+        x *= x;
+    }
+    return x;
+}
+
+
+
