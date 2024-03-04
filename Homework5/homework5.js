7f92d9cb000697521bc321755eaf66d8824bd5b2
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-03-04 14:19:53 +0100

    Ряд Фебоначі

--- Homework5/homework5.js
+++ Homework5/homework5.js
@@ -42,3 +42,14 @@ function sumOrConcatenation(value1, value2) {
     }
 }
 */
+function Fb(number){
+    let a = 0;
+    let b = 1;
+    let c = 0
+    for(let i = 0; i<number; i++){
+    c = a + b;
+    a = b;
+    b = c;
+    }
+    return(c)
+}
