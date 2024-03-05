037a97670db308ea69f5f9863ca4dab3f66d4432
AntonManzhos <160279632+AntonManzhos@users.noreply.github.com>
2024-03-05 10:13:14 +0100

    trinAR OPERATOR IN 12

--- Homework5/homework5.js
+++ Homework5/homework5.js
@@ -10,15 +10,8 @@ function mulBy5(num){
 */
 /*
 function max(num1, num2){
-    if (num1 > num2){
-        return num1;
-    }
-    else if (num1 = num2){
-        return "Числа рівні";
-    }
-    else {
-        return num2;
-    }
+ (num1 > num2 ?  num1 : num2)
+    
 }
 */
 /*
