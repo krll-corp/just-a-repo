7829adb7e1fe5ebe3cf26f09ac129f1808936528
iPad <160279632+AntonManzhos@users.noreply.github.com>
2024-03-22 07:38:20 +0000

    hate

---
+++ Homework9/homework9.js
@@ -0,0 +1,22 @@
+let map = new Map();
+map.set('123n', 'Bigint');
+map.set('true', 'boolian');
+map.set('12', 'number'); 
+map.set("Vasya",'string' );
+map.set("NaN", 'number');
+map.set("false", "boolian");
+map.set("53178319047619n", "bigint");
+map.set("q", "");
+let NewObject = Object.fromEntries(map);
+let map2 = new Map(Object.entries(NewObject));
+function mergeArrays(...arrays) {
+    let mergedArray = new Set();
+
+    arrays.forEach(array => {
+        array.forEach(item => {
+            mergedArray.add(item);
+        });
+    });
+
+    return Array.from(mergedArray);
+}
