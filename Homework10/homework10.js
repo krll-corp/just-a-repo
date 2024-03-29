91e0e123de2987bc96d4b78dc0d71268f0abebb3
iPad <160279632+AntonManzhos@users.noreply.github.com>
2024-03-29 09:54:40 +0000

    Homework10 task1

---
+++ Homework10/homework10.js
@@ -0,0 +1,11 @@
+"use strict"
+let employee1 = {
+  fullName: "Vasya Pupkin",
+  position: "Junior Developer",
+  salary: "500",
+};
+let employee = {
+  fullName: "",
+  position: "",
+  salary: "",
+}
