3190591a5853c5c4dd1f86554c3173684e18a986
iPad <160279632+AntonManzhos@users.noreply.github.com>
2024-04-02 14:44:39 +0000

    first task

--- Homework10/homework10.js
+++ Homework10/homework10.js
@@ -1,11 +1,19 @@
-"use strict"
+  "use strict"
 let employee1 = {
   fullName: "Vasya Pupkin",
   position: "Junior Developer",
   salary: "500",
 };
 let employee = {
-  fullName: "",
-  position: "",
-  salary: "",
+  fullName: "Petya Vaskin",
+  position: "Senior",
+  salary: "2000",
 }
+function showInfo(any_object = {
+  fullName: "nikto",
+  position: "nikto",
+  salary: "0"
+}){
+  console.log();
+}
+apply(emp);
